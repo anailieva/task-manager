7555708d8edaa676af322aa151f9d647e1703470
AnaIlieva <anailieva0149@gmail.com>
2024-01-03 04:15:36 +0200

    DB migrations check.

---
+++ sql/add_tasks.sql
@@ -0,0 +1,4 @@
+insert into TASK (ID, NAME, PRIORITY) values (0, 'Laundry', 'high');
+insert into TASK (ID, NAME, PRIORITY) values (1, 'Dishes', 'high');
+insert into TASK (ID, NAME, PRIORITY) values (2, 'Groceries', 'medium');
+insert into TASK (ID, NAME, PRIORITY) values (3, 'Christmas gifts', 'high');
---
+++ sql/create_task_table.sql
@@ -0,0 +1,6 @@
+create table TASK
+(
+    ID int not null,
+    NAME varchar(256) not null,
+    PRIORITY varchar(100) not null
+);
