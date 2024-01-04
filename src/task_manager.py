0f747546b22081c100d7650f1c0a68521da8603d
AnaIlieva <anailieva0149@gmail.com>
2024-01-04 16:43:19 +0200

    Add more functionality.

--- src/task_manager.py
+++ src/task_manager.py
@@ -1,3 +1,15 @@
 class TaskManager:
     def __init__(self):
         self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append({"task": task, "completed": False})
+
+    def mark_task_completed(self, task_index):
+        if 0 <= task_index < len(self.tasks):
+            self.tasks[task_index]["completed"] = True
+        else:
+            raise IndexError("Invalid task index")
+
+    def get_all_tasks(self):
+        return self.tasks
